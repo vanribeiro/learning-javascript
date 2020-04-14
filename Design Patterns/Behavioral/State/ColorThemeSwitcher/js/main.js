676621b52053aaeb3e6f924910e7203b8a7a2bd0
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-14 02:05:18 -0300

    Need to read more sample and examples.

--- Design Patterns/Behavioral/State/ColorThemeSwitcher/js/main.js
+++ Design Patterns/Behavioral/State/ColorThemeSwitcher/js/main.js
@@ -1,1 +1,12 @@
+/**
+ * Contexto
+ */
 
+
+var body = document.querySelector('body');
+var switchButton = document.querySelector('#switcher-theme-color');
+
+body.onload = () => {
+    console.log("oi");
+    
+}
